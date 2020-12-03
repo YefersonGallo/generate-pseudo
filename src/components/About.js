64b0fd46abebdf97d3d1c8cda4051ef95883aa8f
YefersonGallo <yeferson.gallo@uptc.edu.co>
2020-12-03 11:30:53 -0500

    Corrección About

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ function About() {
     return (
         <div className="box m-5">
             <h1 className="title is-1"> Acerca de</h1>
-            <p>Esta aplicación se hace como parte del primer taller de la asignatura Simulación por Computdor, de la escuela de Ingeniería de Sistemas y Computación de la
+            <p>Esta aplicación se hace como parte del primer taller de la asignatura Simulación por Computador, de la escuela de Ingeniería de Sistemas y Computación de la
              Universidad Pedagógica y Tecnológica de Colombia</p>
             <h2 className="title is-2">Integrantes</h2>
             <div className="control is-horizontal">
